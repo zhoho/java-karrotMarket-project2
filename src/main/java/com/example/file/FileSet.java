461685e170aec25923f53c1322ae8c5563a31122
Zho <chlwlgh1011@naver.com>
2024-01-03 21:30:00 +0900

    feat: 파일 path 설정

--- src/main/java/com/example/file/FileSet.java
+++ src/main/java/com/example/file/FileSet.java
@@ -3,9 +3,11 @@ package com.example.file;
 import java.nio.file.Paths;
 
 public class FileSet {
+    public static String filename;
+    public static String path;
     public static void setFileDir() {
-        String path = Paths.get(".").toAbsolutePath().toString();
-        String filename = path + "/data.txt";
-        FileSave.saveFile(filename);
+        path = Paths.get("src/main/java/com/example/").toAbsolutePath().toString();
+        filename = path + "/Item.json";
+        FileLoad.loadItemsFromFile();
     }
 }
