2b3c39d651dd4358695e13f10f80dc0336047a2c
Zho <chlwlgh1011@naver.com>
2024-01-03 21:29:42 +0900

    feat: local 데이터를 Json으로 저장하는 FileSave 구현

--- src/main/java/com/example/file/FileSave.java
+++ src/main/java/com/example/file/FileSave.java
@@ -1,19 +1,32 @@
 package com.example.file;
 
+import com.example.item.Item;
+import com.example.constant.Constant;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import static com.example.item.Item.items;
+import static com.example.file.FileSet.filename;
 
 public class FileSave {
-    public static void saveFile(String filename) {
-        PrintWriter printWriter = null;
-        try {
-            printWriter = new PrintWriter(new FileWriter(filename));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public static void saveItemToFile() {
+        JSONArray jsonArray = new JSONArray();
+        for (Item item : items) {
+            JSONObject itemJson = new JSONObject();
+            itemJson.put("id", item.getId());
+            itemJson.put("seller", item.getSeller());
+            itemJson.put("itemName", item.getItemName());
+            itemJson.put("price", item.getPrice());
+            itemJson.put("dateTime", item.getDateTime());
+            jsonArray.put(itemJson);
         }
-        printWriter.write("test");
-        printWriter.close();
+
+        try (FileWriter file = new FileWriter(filename)) {
+            file.write(jsonArray.toString(Constant.FOUR));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
